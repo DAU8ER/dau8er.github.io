9c6b132479d733798cf4ea0ed703b47920a7a912
DAU8ER <115318168+DAU8ER@users.noreply.github.com>
2022-12-26 14:38:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("tf").addEventListener("click", () => {
+    scrollTo({top: 0, behavior: "smooth"})
+});
+
+// let mdata = require("https://modlookup.3v.fi/api/user-totals/Dau8er")
+//window.onscroll = (ev) => {
+    // console.log(`${scrollY} ${window.innerHeight} ${window.outerHeight-scrollY}\t${window.innerWidth} ${window.innerHeight} ${window.innerWidth/window.innerHeight} ${Math.round(window.innerWidth/window.innerHeight) }`)
+   // let scrollnum = (Math.round(window.innerWidth/window.innerHeight) === 2 ? 80 : 210);
+    // 207
+    //if(window.outerHeight-scrollY <= scrollnum){
+      //  console.log(":tf:")
+        //scrollTo({top:0, behavior: "smooth"});
+    //}
